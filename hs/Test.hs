d9a3f9522383839e82189b4cce61f0d4800c5990
Frano Perleta <frane.perleta@gmail.com>
2014-02-14 17:57:51 +0100

    It works. :)

--- hs/Test.hs
+++ hs/Test.hs
@@ -24,13 +24,17 @@ score = scale (Sec $ 60 / 163) $ do
     fw <- fwriter2Make "test.aif" outL outR
 
     master <- makeSum
+    touch master
     wireMake master outL 1
     wireMake master outR 1
 
-    shift (Cell 0) $ pluck master 1 (Hz 166) (Hz 16) (Sec 0.125)
-    shift (Cell 1) $ pluck master 0.7 (Hz 220) (Hz 133) (Sec 0.15)
-    shift (Cell 2) $ pluck master 1 (Hz 166) (Hz 16) (Sec 0.125)
-    shift (Cell 3) $ pluck master 0.7 (Hz 166) (Hz 16) (Sec 0.125)
+    frame (Cell 4) $ do
+        shift (Cell 0.5) $ pluck master 0.3 (Hz 220) (Hz 220) (Cell 3)
+        shift (Cell 1.5) $ pluck master 0.3 (Hz 440) (Hz 440) (Cell 2.5)
+        shift (Cell 0) $ pluck master 1 (Hz 166) (Hz 16) (Sec 0.125)
+        shift (Cell 1) $ pluck master 1 (Hz 220) (Hz 16) (Sec 0.15)
+        shift (Cell 2.5) $ pluck master 0.7 (Hz 166) (Hz 16) (Sec 0.125)
+        shift (Cell 3) $ pluck master 1 (Hz 220) (Hz 16) (Sec 0.15)
 
 pluck :: Ref s P -> Double -> Freq -> Freq -> Time -> Score s ()
 pluck out amp f0 f1 dur = frame dur $ do
@@ -49,7 +53,10 @@ pluck out amp f0 f1 dur = frame dur $ do
     fenv <- envMake fc f0'
     envXdec fenv f1' (dur' / 5)
 
-    aenv <- envMake xx 1
-    envXdec aenv 0 (dur' / 5)
+    let att = Sec 0.0025
+    att' <- realToFrac <$> toPeriods att
+    aenv <- envMake xx 0
+    envLin aenv 1 att'
+    shift att $ envXdec aenv 0 ((dur' - att') / 5)
 
 -- vim:fdm=marker:
